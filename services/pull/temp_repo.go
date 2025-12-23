28602a22fa41cf8bf477ff39033b061486f69e48
gpi <gpimpale29@gmail.com>
2025-12-23 02:20:43 +0000

    Fix creating pull request failure when target branch name same as tag
    Use full reference name (refs/heads/) in the git command to avoid ambiguity
    when a tag exists with the same name as the branch.
    
    Related: PR #35552

--- services/pull/temp_repo.go
+++ services/pull/temp_repo.go
@@ -140,7 +140,7 @@ func createTemporaryRepoForPR(ctx context.Context, pr *issues_model.PullRequest)
 	}
 
 	if err := prCtx.PrepareGitCmd(gitcmd.NewCommand("fetch", "origin").AddArguments(fetchArgs...).
-		AddDashesAndList(pr.BaseBranch+":"+baseBranch, pr.BaseBranch+":"+"original_"+baseBranch)).
+		AddDashesAndList(git.BranchPrefix+pr.BaseBranch+":"+git.BranchPrefix+baseBranch, git.BranchPrefix+pr.BaseBranch+":"+git.BranchPrefix+"original_"+baseBranch)).
 		Run(ctx); err != nil {
 		log.Error("%-v Unable to fetch origin base branch [%s:%s -> base, original_base in %s]: %v:\n%s\n%s", pr, pr.BaseRepo.FullName(), pr.BaseBranch, tmpBasePath, err, prCtx.outbuf.String(), prCtx.errbuf.String())
 		cancel()
